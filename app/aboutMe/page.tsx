7a09eaaf003145845140997b057606d93c8deacd
ccretterer <carly.c.retterer.24@dartmouth.edu>
2024-02-22 15:59:49 -0500

    updating apostrophes

--- app/aboutMe/page.tsx
+++ app/aboutMe/page.tsx
@@ -8,8 +8,8 @@ export default function aboutMe(){
 
             <div> 
                 {/* //description summary */}
-                I'm a current senior at Dartmouth College, majoring in Government and Computer Science. 
-                As I approach the end of my undergraduate education in November 2024, I'm actively seeking 
+                I&aposm a current senior at Dartmouth College, majoring in Government and Computer Science. 
+                As I approach the end of my undergraduate education in November 2024, I&aposm actively seeking 
                 new graduate positions where I can make meaningful contributions and further my growth in areas 
                 that deeply interest me: technology, cybersecurity, national security, and sustainability.
             </div>
