e4240f0e35b7bdd58b0bdf75a1ed34367184a558
ccretterer <carly.c.retterer.24@dartmouth.edu>
2024-02-24 11:59:38 -0500

    edit to projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,58 +1,39 @@
-
-import Carousel from "../components/images/carousel";
-
 export default function Projects() {
     return (
-        <div className="ml-8"> {/* Adjust ml-8 to increase or decrease the margin */}
-            hi im here
-
-            <ol className="items-center sm:flex">
-                <li className="relative mb-6 sm:mb-0">
-                    <div className="flex items-center">
-                        <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                            </svg>
+        <div className="px-8 py-12">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                {projects.map((project, index) => (
+                    <div key={index} className="group rounded-xl overflow-hidden shadow-xl transform hover:scale-105 transition-transform duration-500 ease-in-out">
+                        <div className="p-6 bg-gradient-to-r from-gray-700 via-gray-900 to-black">
+                            <h3 className="text-2xl font-bold text-white mb-3">{project.title}</h3>
+                            <time className="block mb-2 text-lg text-gray-300">{project.date}</time>
+                            <p className="text-gray-300 text-lg">{project.description}</p>
                         </div>
-                        <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                     </div>
-                    <div className="mt-3 sm:pe-8">
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.0.0</h3>
-                        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 2, 2021</time>
-                        <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-                    </div>
-                </li>
-                <li className="relative mb-6 sm:mb-0">
-                    <div className="flex items-center">
-                        <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                            </svg>
-                        </div>
-                        <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                    </div>
-                    <div className="mt-3 sm:pe-8">
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.0</h3>
-                        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 23, 2021</time>
-                        <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-                    </div>
-                </li>
-                <li className="relative mb-6 sm:mb-0">
-                    <div className="flex items-center">
-                        <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                            </svg>
-                        </div>
-                        <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                    </div>
-                    <div className="mt-3 sm:pe-8">
-                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.3.0</h3>
-                        <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 5, 2022</time>
-                        <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements.</p>
-                    </div>
-                </li>
-            </ol>
+                ))}
+            </div>
         </div>
     );
 }
+
+const projects = [
+    {
+        title: "Tiny Search Engine",
+        date: "Released in Spring 2023",
+        description: "Designed with subsystems: crawler, indexer, and querier.",
+        image: "https://res.cloudinary.com/djui88jlr/image/upload/v1708792417/web-icon-line-on-white-background-image-for-web-presentation-logo-icon-symbol-free-vector_zz97ig.jpg" // Placeholder image URL
+    },
+    {
+        title: "A Cost-Benefit Analysis of Securing Build Environments",
+        date: "Started in Fall 2023",
+        description: "Comparing isolated, hermetic, reproducible, and bootstrappable builds in achieving desired levels of build environment security.",
+        image: "https://res.cloudinary.com/djui88jlr/image/upload/v1708634190/IMG_1059_meixom.jpg" // Placeholder image URL
+    },
+    {
+        title: "PowerPoint Data Extraction and LLMs",
+        date: "Released in November 2023",
+        description: "Developed a RAG that extracted data from a PowerPoint, summarized data chunks using GPT-4, stored using ChromaDB, and designed a querying process where a user can ask questions about the extracted data using GPT-4.",
+        image: "https://res.cloudinary.com/djui88jlr/image/upload/v1708793205/EXTRACT_DATA_FROM_agilyi.png" // Placeholder image URL
+    },
+    // ... more projects
+];
